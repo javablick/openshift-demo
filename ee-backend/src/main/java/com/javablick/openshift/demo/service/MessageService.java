28da04aed0796fc56a6e8ef4128cac7e69e11d1e
Java-Blick <java.blick@gmail.com>
2018-08-25 18:52:47 +0200

    Simple war module with rest interface added

---
+++ ee-backend/src/main/java/com/javablick/openshift/demo/service/MessageService.java
@@ -0,0 +1,24 @@
+package com.javablick.openshift.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("message")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class MessageService {
+
+	@GET
+	public List<String> getMessages() {
+		List<String> result = new ArrayList<>();
+		result.add("test");
+		return result;
+	}
+
+}
